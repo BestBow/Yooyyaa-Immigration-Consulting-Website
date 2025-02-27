88fc1b41a59e980c96e3a567b92ca5a771403774
Malk Daboor <malk.daboor@dal.ca>
2025-02-27 11:53:24 -0400

    started about us

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import AboutUs from './Components/AboutUs'
 
 function App() {
 
   return (
     <>
       this is a plain start.
+      <AboutUs />
     </>
   )
 }
---
+++ client/src/Components/AboutUs.jsx
@@ -0,0 +1,14 @@
+import '../Styles/AboutUs.css';
+
+function AboutUs() {
+    return (
+        <div className="container">
+            <div className="hero">
+                <h1>ABOUT US</h1>
+                <h2>WHO WE ARE & WHAT WE DO</h2>
+            </div>
+        </div>
+    );
+}
+
+export default AboutUs;
