c41499356e624b857e74e6c5c8337a9f96a93957
Ali Amini <al935400@dal.ca>
2025-02-28 18:36:32 -0400

    Updated Global CSS file by adding breakpoints for tablet and mobile views, and dded font sizes for headers and paragraphs

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function App (){
+    return (
+        <>
+        <h1>Yellow header 1</h1>
+        <h3>Blue Header</h3>
+        <p>Paragraph</p>
+        </>
+    )
+}
+
+
