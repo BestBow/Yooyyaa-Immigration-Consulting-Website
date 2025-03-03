36a7f1416bd495e08d3a5461023f3438c3b01990
Ali Amini <al935400@dal.ca>
2025-03-03 15:12:12 -0400

    Installed tailwind and added tailwind imports to global css file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react"
-
+import "./styles/global.css"
 export default function App (){
     return (
         <>
-        <h1>Yellow header 1</h1>
+        <h1 className="flex items-center bg-blue-500 px-4 py-3 text-white hover:bg-blue-400">Yellow header 1</h1>
         <h3>Blue Header</h3>
         <p>Paragraph</p>
         <button>Hello</button>
