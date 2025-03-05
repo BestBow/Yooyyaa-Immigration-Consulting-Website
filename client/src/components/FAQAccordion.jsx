5dd6076d99baefac82d78ffe3ac2ce3f12f0f4a8
Ali Amini <al935400@dal.ca>
2025-03-05 18:30:39 -0400

    implementation: completed logic for FAQAccordion Page

--- client/src/components/FAQAccordion.jsx
+++ client/src/components/FAQAccordion.jsx
@@ -1,26 +1,59 @@
-import React,{useState} from "react";
-
-
-let AccordionItem = (props) => {
-	return(
-		<>
-		</>
-	);
-}
+import React, { useState } from "react";
 /*
-	Component gets every Q&A accordion item from a list prop
-	and passes it to AccordionItem to handle the logic and display
-	for each individual Accordion item. An accordion item is a block
-	section that consists of a question and answer.
+ Handles the logic for displaying each item in Q&A List, and uses re-rendering to conditonally
+ display the answer section. Using the useState hook updatePanel conditionally displays the answer
+ block. 
+ */
+let AccordionItem = ({question, answer}) => {
+  const [isPanelOpen, updatePanel] = useState(false); 
+
+  return (
+    <div>
+			<div>{question}</div>
+			
+			<svg className=""
+        onClick={() => updatePanel(!isPanelOpen)}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="black"
+      >
+				<path
+        	strokeLinecap="round"
+        	strokeLinejoin="round"
+        	strokeWidth="1"
+        	d={isPanelOpen ? "M5 15l6-6 6 6" : "M19 9l-6 6-6-6"}
+      	/>
+			</svg>
+		
+			{isPanelOpen && <div>{answer}</div>} 
+    </div>
+  );
+}
+
+/*
+  Component gets every Q&A accordion item from a list prop
+  and passes it to AccordionItem to handle the logic and display
+  for each individual Accordion item. An accordion item is a block
+  section that consists of a question and answer.
 */
-export default function FAQAccordion(props) {
-  <div>
-  	{props.map((qna) =>(
-    	<AccordionItem 
-        question={qna.question}
-        answer={qna.answer}
-        key= {crypto.randomUUID()} //create & pass a unique key for each item
-			/>
-    ))}
-  </div>
+export default function FAQAccordion({ faq }) {
+  
+	if(!faq){
+		return <>Rendering plz wait</>
+	}
+	
+	return (
+		<div>
+      {faq.map((qna) => (
+        <AccordionItem 
+          question={qna.question}
+          answer={qna.answer}
+          key={crypto.randomUUID()} // create & pass a unique key for each item
+        />
+      ))}
+    </div>
+  );
 }
