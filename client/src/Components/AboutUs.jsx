c62f2421dd0827047972931eb297d29e40247553
Malk Daboor <malk.daboor@dal.ca>
2025-03-06 12:55:00 -0400

    mobile view mostly done

--- client/src/Components/AboutUs.jsx
+++ client/src/Components/AboutUs.jsx
@@ -13,9 +13,10 @@ function AboutUs() {
             <span className="line mt-4 mb-4"></span>
             <p id="blurb">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             
-            <div className="mission-vision">
+            <div className="company">
                 <h3>COMAPNY MISSION & VISION</h3>
-                <div className="mission-vision grid grid-cols-2 gap-4">
+                <span className="mobile-line"></span>
+                <div className="mission-vision grid grid-cols-2 gap-4 mr-4 ml-4">
                     <div className="mission">
                         <h5>OUR MISSION</h5>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra.</p>
