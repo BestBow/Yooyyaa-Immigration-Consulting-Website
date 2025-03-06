268456844d0a49e4ab30639eab8506dbe3555474
Malk Daboor <malk.daboor@dal.ca>
2025-03-06 12:31:21 -0400

    About us desktop complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,10 +7,6 @@ function App() {
   return (
     <>
       <AboutUs />
-      <h1 className="flex items-center bg-blue-500 px-4 py-3 text-white hover:bg-blue-400">Yellow header 1</h1>
-      <h3>Blue Header</h3>
-      <p>Paragraph</p>
-      <button>Hello</button>
     </>
   )
 }
--- client/src/Components/AboutUs.jsx
+++ client/src/Components/AboutUs.jsx
@@ -4,11 +4,10 @@ import face from '../Assets/placeholder.jpeg';
 
 function AboutUs() {
     return (
-        <div className="container grid content-center">
-            
-            <div className="hero flex flex-col items-center justify-center h-screen">
+        <div className="container mt-10 flex flex-col justify-center">
+            <div className="hero flex flex-col items-center justify-center">
                 <h1 className="mb-4 text-4xl font-extrabold">ABOUT US</h1>
-                <p className="text-s text-[#FFC700]"><b>WHO WE ARE & WHAT WE DO</b></p>
+                <p className="text-s"><b>WHO WE ARE & WHAT WE DO</b></p>
             </div>
 
             <span className="line mt-4 mb-4"></span>
@@ -16,7 +15,7 @@ function AboutUs() {
             
             <div className="mission-vision">
                 <h3>COMAPNY MISSION & VISION</h3>
-                <div className="mission-vision grid grid-cols-2">
+                <div className="mission-vision grid grid-cols-2 gap-4">
                     <div className="mission">
                         <h5>OUR MISSION</h5>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra.</p>
@@ -28,39 +27,39 @@ function AboutUs() {
                 </div>
             </div>
 
-            <div className="meet-our-experts">
+            <div className="meet-our-experts flex flex-col items-center justify-center place-content-center mb-20">
                 <h3>MEET OUR EXPERTS</h3>
                 <span className="line mt-4 mb-4"></span>
                 <div className="experts">
-                    <div className="expert">
+                    <div className="expert flex flex-col items-center" id="expert1">
                         <img src={face} alt="one of our consultants"/>
                         <p><b>John Doe, RCIC #123456<br />
                         Senior Immigration Consultant</b><br />
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore.
                         </p>
                     </div>
-                    <div className="expert">
+                    <div className="expert flex flex-col items-center" id="expert2">
                         <img src={face} alt="one of our consultants"/>
                         <p><b>John Doe, RCIC #123456<br />
                         Senior Immigration Consultant</b><br />
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore.
                         </p>
                     </div>
-                    <div className="expert">
+                    <div className="expert flex flex-col items-center" id="expert3">
                         <img src={face} alt="one of our consultants"/>
                         <p><b>John Doe, RCIC #123456<br />
                         Senior Immigration Consultant</b><br />
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore.
                         </p>
                     </div>
-                    <div className="expert">
+                    <div className="expert flex flex-col items-center" id="expert4">
                         <img src={face} alt="one of our consultants"/>
                         <p><b>John Doe, RCIC #123456<br />
                         Senior Immigration Consultant</b><br />
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore.
                         </p>
                     </div>
-                    <div className="expert">
+                    <div className="expert flex flex-col items-center" id="expert5">
                         <img src={face} alt="one of our consultants"/>
                         <p><b>John Doe, RCIC #123456<br />
                         Senior Immigration Consultant</b><br />
@@ -70,7 +69,7 @@ function AboutUs() {
                 </div>
             </div>
 
-            <div className="why-us">
+            <div className="why-us space-y-4 flex flex-col">
                 <h2>WHY CHOOSE US</h2>
                 <span className="line mt-4 mb-4"></span>
                 <p>✅ <b>Licensed & Certified Experts – Lorem ipsum dolor sit amet, consectetur adipiscing elit</b></p>
@@ -79,8 +78,10 @@ function AboutUs() {
                 <p>⚡ <b>Fast & Reliable Service – Lorem ipsum dolor sit amet, consectetur adipiscing elit</b></p>
             </div>
 
-            <h2>Ready to start your journey? Let's make it happen!</h2>
-            <button id="get-in-touch">GET IN TOUCH</button>
+            <div className="connect mb-20">
+                <h2 className="ready mt-20 mb-10 text-3xl">Ready to start your journey? Let's make it happen!</h2>
+                <button type="button" id="get-in-touch">GET IN TOUCH</button>
+            </div>
         </div>
     );
 }
