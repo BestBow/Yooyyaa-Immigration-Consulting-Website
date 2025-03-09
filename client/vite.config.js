a78faf75a482e02fd800c38a8d30b8fc10d104d2
ivagba <om754768@dal.ca>
2025-03-09 14:24:41 -0300

    fixed error

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
-
-
-=======
-import tailwindcss from '@tailwindcss/vite'
->>>>>>> develop
 // https://vite.dev/config/
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-<<<<<<< HEAD
-  plugins: [react(), tailwindcss()],
-
-=======
   plugins: [react(), tailwindcss()]
->>>>>>> develop
 })
