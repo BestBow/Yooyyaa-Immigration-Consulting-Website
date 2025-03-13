d92e280ba045d95237ea74657c7510d882489119
ivagba <om754768@dal.ca>
2025-03-13 12:12:49 -0300

    fixed header and footer

--- client/src/Components/footer.jsx
+++ client/src/Components/footer.jsx
@@ -14,27 +14,27 @@ const Footer = () => {
         {/* Logo */}
         <div className="footer-logo-container">
           <Link to="/">
-            {" "}
-            <img src={logoWname} alt="Logo" className="footer-logo" />{" "}
+            <img src={logoWname} alt="Logo" className="footer-logo" />
           </Link>
         </div>
 
         {/* Contact Details */}
         <div className="footer-info">
           <div className="footer-item">
-
-          <img className="bg-[#FFC700] rounded-full" src={locationIcon} />
-
+            {/* Icon with yellow background and rounded corners */}
+            <img className="bg-[#FFC700] p-3 rounded-full" src={locationIcon} alt="Location" />
             <p className="mt-2">
-              123 This Street <br /> Halifax, Nova Scotia <br /> A1A 1A1
+              123 This Street <br />
+              Halifax, Nova Scotia <br />
+              A1A 1A1
             </p>
           </div>
           <div className="footer-item">
-          <img className="bg-[#FFC700] rounded-full" src={mailIcon} />
+            <img className="bg-[#FFC700] p-3 rounded-full" src={mailIcon} alt="Email" />
             <p className="mt-2">something@gmail.com</p>
           </div>
           <div className="footer-item">
-          <img className="bg-[#FFC700] rounded-full" src={phoneIcon} />
+            <img className="bg-[#FFC700] p-3 rounded-full" src={phoneIcon} alt="Phone" />
             <p className="mt-2">902-111-2222</p>
           </div>
         </div>
@@ -46,10 +46,11 @@ const Footer = () => {
       {/* Bottom Section - Links & Copyright */}
       <div className="footer-bottom">
         <div className="footer-links">
-          <Link to="/termsAndPrivacy">Terms and Privacy Policy</Link>
+          <Link to="/privacy">Privacy Policy</Link>
+          <Link to="/terms">Terms &amp; Conditions</Link>
         </div>
         <p className="copyright">
-          &copy; 2025 Yooyaa Immigration Consultants. All rights reserved.
+          &copy; 2025 Yooyyaa Immigration Consultants. All rights reserved.
         </p>
       </div>
     </footer>
