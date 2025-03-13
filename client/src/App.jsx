08a39a3319f61f04a92130cd3681652d55b93e52
Stephanie Rowe <serowe@dal.ca>
2025-03-13 12:46:01 -0300

    Committed to pull some changes from dev

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import "./styles/global.css"
-import AboutUs from './Components/AboutUs'
-import Services from './pages/Services.jsx'
-import Homepage from "./pages/Homepage.jsx";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./Styles/global.css";
 
-function App (){
-    return (
-        <>
-          <AboutUs />
-          <Services />
-          <Homepage />
-        </>
-        
-    )
+import Header from "./Components/header";
+import Footer from "./Components/footer";
+import Homepage from "./pages/Homepage";
+import AboutUs from "./components/AboutUs";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import FAQ from "./pages/FAQ";
+import TermsandPrivacyPolicy from "./Components/termsAndPrivacyPolicy";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/termsAndPrivacy" element={<TermsandPrivacyPolicy />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
   export default App
