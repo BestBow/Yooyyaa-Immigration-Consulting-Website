80c2c8b7316e923436e8a1253f917778d33f106c
ivagba <om754768@dal.ca>
2025-03-20 12:50:16 -0300

    fixed the uI and added the heading banner

--- client/src/Components/headingBanner.tsx
+++ client/src/Components/headingBanner.tsx
@@ -1,30 +1,41 @@
 import React from "react";
 
-export default function HeadingBanner ( title: string | TrustedHTML, bannerPhoto: string | null )
-{
-	return (
-		<div className="w-full relative flex flex-col items-center">
-			<div className="w-screen absolute left-1/2 transform -translate-x-1/2">
-				<img
-					src={ bannerPhoto != null ? bannerPhoto : '../assets/service-banner.jpg' }
-					alt="Banner"
-					className="w-full h-[400px] object-cover"
-				/>
+export default function HeadingBanner(title: string | TrustedHTML, bannerPhoto: string | null) {
+  return (
+    <div className="w-full relative flex flex-col items-center mb-8">
+      <div className="w-screen absolute left-1/2 transform -translate-x-1/2">
+        <img
+          src={bannerPhoto != null ? bannerPhoto : "../assets/service-banner.jpg"}
+          alt="Banner"
+          className="w-full h-[400px] object-cover"
+        />
 
-				<div className="
-					absolute inset-0
-					bg-gradient-to-t 
-					from-[#FFFFFF] from-0%
-					to-[#003580]/25 to-80%
-				">
-					<h1 className='banner-title absolute inset-x-0 top-1/3 text-center'
-						dangerouslySetInnerHTML={{ __html: title? title : 'PAGE<br/>TITLE' }}
-					>
-					</h1>
-				</div>
-			</div>
-			{/* Spacer to maintain layout flow */ }
-			<div className="h-[350px]"></div>
-		</div>
-	);
+        <div
+          className="
+            absolute inset-0
+            bg-gradient-to-t 
+            from-[#FFFFFF] from-0%
+            to-[#003580]/50 to-80%
+          "
+        >
+          <h1
+            className="
+              banner-title 
+              absolute inset-x-0 top-1/3 
+              text-center 
+              text-5xl md:text-6xl 
+              font-extrabold 
+              uppercase 
+              text-yellow-400
+            "
+            style={{ textShadow: "2px 2px 6px rgba(0, 0, 0, 0.8)" }}
+            dangerouslySetInnerHTML={{ __html: title ? title : "PAGE<br/>TITLE" }}
+          />
+        </div>
+      </div>
+
+      {/* Spacer to push subsequent content below the banner */}
+      <div className="h-[350px]" />
+    </div>
+  );
 }
