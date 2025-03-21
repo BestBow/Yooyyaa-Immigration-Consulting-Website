0967c18073ac428a0d7f53f73fc1d12346f08291
Ali Amini <al935400@dal.ca>
2025-03-21 20:34:46 -0300

    Homepage Main Title is now all caps for consistency across pages

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -24,7 +24,7 @@ const Homepage = () => {
     <div>
       {/* Hero Section */}
       <section className="hero-section">
-        <h1>Home</h1>
+        <h1>HOME</h1>
       </section>
 
       {/* Services Section */}
