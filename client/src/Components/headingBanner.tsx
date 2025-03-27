bc06dce95aac8ab418ece209e9f847773bc535d5
JBai34 <jc_bai@outlook.com>
2025-03-27 12:48:12 -0300

    Feat!: added subtitle to headingBanner

--- client/src/Components/headingBanner.tsx
+++ client/src/Components/headingBanner.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 const DEFAULT_BANNER_PHOTO = 'https://4kwallpapers.com/images/walls/thumbs_3t/3939.jpg';
 
-export default function HeadingBanner ( title: string | TrustedHTML, bannerPhoto: string | null )
-{
-	return (
-		<div className="w-full relative flex flex-col items-center">
-			<div className="w-screen absolute left-1/2 transform -translate-x-1/2">
-				<img
-					src={bannerPhoto != null ? bannerPhoto : DEFAULT_BANNER_PHOTO }
-					alt="Banner"
-					className="w-full h-[400px] object-cover"
-				/>
+export default function HeadingBanner(title: string | TrustedHTML, subtitle?: string | TrustedHTML, bannerPhoto?: string) {
+  return (
+    <div className="w-full relative flex flex-col items-center mb-30">
+      <div className="w-screen absolute left-1/2 transform -translate-x-1/2">
+        <img
+          src={bannerPhoto != null ? bannerPhoto : DEFAULT_BANNER_PHOTO}
+          alt="Banner"
+          className="w-full h-[500px] object-cover"
+        />
 
         <div
           className="
@@ -20,19 +19,28 @@ export default function HeadingBanner ( title: string | TrustedHTML, bannerPhoto
             to-[#003580]/50 to-80%
           "
         >
-          <h1
-            className="
-              banner-title 
-              absolute inset-x-0 top-1/3 
+          <div className="flex flex-col justify-center items-center h-full">
+            <h1
+              className=" 
+              inset-x-0
               text-center 
-              text-5xl md:text-6xl 
-              font-extrabold 
-              uppercase 
-              text-yellow-400
+              heading-banner-h1
             "
-            style={{ textShadow: "2px 2px 6px rgba(0, 0, 0, 0.8)" }}
-            dangerouslySetInnerHTML={{ __html: title ? title : "PAGE<br/>TITLE" }}
-          />
+              style={{ textShadow: "3px 3px 8px rgba(0, 0, 0, 0.7)" }}
+              dangerouslySetInnerHTML={{ __html: title ? title : "PAGE TITLE" }}
+            />
+
+            <h2
+              className=" 
+              inset-x-0
+              text-center 
+              heading-banner-h2
+            "
+              style={{ textShadow: "3px 3px 6px rgba(0, 0, 0, 0.7)" }}
+              dangerouslySetInnerHTML={{ __html: subtitle ? subtitle : "Dedicated to making your immigration journey smooth and successful!" }}
+            />
+          </div>
+
         </div>
       </div>
 
